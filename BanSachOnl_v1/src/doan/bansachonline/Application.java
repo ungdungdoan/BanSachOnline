a489f904fb242d2e4601fa981ffcda954b3f0572
VoThanhPhuc <phuc.vothanh96@gmail.com>
2016-10-25 19:35:31 +0700

    Them mot so file moi

--- BanSachOnl_v1/src/doan/bansachonline/Application.java
+++
@@ -1,7 +0,0 @@
-package doan.bansachonline;
-
-public class Application {
-	public static void main(String[] args) {
-		new FormQuanLyDauSach().setVisible(true);
-	}
-}
--- New_Project/src/doan/bansachonline/TestconnectSql.java
+++
@@ -1,9 +0,0 @@
-package doan.bansachonline;
-
-public class TestconnectSql {
-	public static void main(String[] args) {
-		DanhMucSach dms = new DanhMucSach("013","Conan","100ngd");
-		dms.create();
-		System.out.println("Connect");
-	}
-}
